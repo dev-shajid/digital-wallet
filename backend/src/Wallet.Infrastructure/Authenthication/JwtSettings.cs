49cba7a7df7b2eac23e50d6b2db2a080256d0cb3
A. M. Arka <amarka310@gmail.com>
2026-09-22 19:15:23 +0600

    fixed jwt secret-key bug

--- backend/src/Wallet.Infrastructure/Authenthication/JwtSettings.cs
+++ backend/src/Wallet.Infrastructure/Authenthication/JwtSettings.cs
@@ -2,7 +2,7 @@ namespace WalletSystem.Infrastructure.Authentication;
 
 public class JwtSettings
 {
-    public string Secret { get; set; } = string.Empty;
+    public string Key { get; set; } = string.Empty;
 
     public string Issuer { get; set; } = string.Empty;
 
