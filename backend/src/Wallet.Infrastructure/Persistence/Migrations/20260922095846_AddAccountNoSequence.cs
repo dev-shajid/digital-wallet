7118743db4e1c1ec30e6208928e5f5c25ac54308
dev-shajid <sajidislam729@gmail.com>
2026-09-22 23:40:05 +0600

    feat: add sequence for account number generation and update related migration files

---
+++ backend/src/Wallet.Infrastructure/Persistence/Migrations/20260922095846_AddAccountNoSequence.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Wallet.Infrastructure.Persistence.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddAccountNoSequence : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateSequence(
+                name: "account_no_seq",
+                startValue: 100000001L);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropSequence(
+                name: "account_no_seq");
+        }
+    }
+}
